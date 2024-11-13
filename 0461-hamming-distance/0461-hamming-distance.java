7f297d3e58c02275bc30e61c38813fb16aff3ce7
guddenro <kv1jn7@gmail.com>
2024-11-13 21:58:11 +0530

    Time: 0 ms (100%), Space: 40.1 MB (83.24%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,11 +1,20 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int count = 0;
-        for(int i = 0 ; i < 32; i++){
-            if( ((x>>i)&1) !=   ((y>>i)&1)){
-                count++;
-            }
-        }
-        return count;
+        // int count = 0;
+        // for(int i = 0 ; i < 32; i++){
+        //     if( ((x>>i)&1) !=   ((y>>i)&1)){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        int res = 0;
+    int m = x^y;                  // take the xor of two numbers
+    while(m != 0){                // count the no of "1"s 
+        if((m&1) == 1)
+            res++;
+        m = m>>1;
+    }
+    return res;
     }
 }
